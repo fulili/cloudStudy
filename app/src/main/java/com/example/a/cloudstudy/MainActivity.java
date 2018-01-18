5472cc90cdc7f3b9b4aa7455c56c793d5d3aa0da
foley <ADcloundjfl123>
2018-01-18 12:47:27 +0800

    tijiao

--- app/src/main/java/com/example/a/cloudstudy/MainActivity.java
+++ app/src/main/java/com/example/a/cloudstudy/MainActivity.java
@@ -20,18 +20,6 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.example.a.cloudstudy.fragments.FirstFragment;
 import com.example.a.cloudstudy.fragments.FourthFragment;
